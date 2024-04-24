3d2bedbfe97be3f6edd9cf0a7093f3637023aa79
ProGaMEr110521 <vladtem3943@gmail.com>
2024-04-24 11:26:50 +0300

    Loading and adding new achievements into the site using *.txt

--- app/main.py
+++ app/main.py
@@ -1,31 +1,51 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import json
+import logging
+import sqlite3
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates/html')
 
 
-@app.get('/achievements', response_class=HTMLResponse)
+def load_achievements_from_file(file_path):
+    achievements = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            try:
+                name, description = line.strip().split(':')
+                achievements[name.strip()] = description.strip()
+            except:
+                logging.error("Error loading achievements from file")
+    return achievements
+
+
+
+achievements_data = load_achievements_from_file('achievements.txt')
+achievements_json = json.dumps(achievements_data, ensure_ascii=False, indent=4)
+
+
+@app.get('/new_achievement', response_class=HTMLResponse)
 async def show_achievements_form(request: Request):
     return templates.TemplateResponse('achievements_form.html', {'request': request})
 
 
-# Обработка данных из формы
+@app.get('/achievements', response_class=HTMLResponse)
+async def show_achievements(request: Request):
+    achievements_data = load_achievements_from_file('achievements.txt')
+    return templates.TemplateResponse('achievements.html', {'request': request, 'achievements': achievements_data})
+
+
+def save_achievement_to_file(achievement_name, achievement_description):
+    with open('achievements.txt', 'a') as file:
+        file.write(f"{achievement_name}: {achievement_description}\n")
+
+
 @app.post('/add_achievement')
 async def add_achievement(
         achievement_name: str = Form(...),
-        achievement_description: str = Form(...)
-):
-    # Здесь можно добавить логику для сохранения достижения в базе данных или другие действия
-    # Например:
-    # save_achievement_to_database(achievement_name, achievement_description)
+        achievement_description: str = Form(...)):
+    save_achievement_to_file(achievement_name, achievement_description)
 
     return {'message': f'Достижение "{achievement_name}" успешно добавлено!'}
-
-# Пример HTML-шаблона (achievements_form.html)
-# <form method="post" action="/add_achievement">
-#     <input type="text" name="achievement_name" placeholder="Название достижения">
-#     <input type="text" name="achievement_description" placeholder="Описание достижения">
-#     <button type="submit">Добавить</button>
-# </form>
